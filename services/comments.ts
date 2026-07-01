b0791f5bd9233e80a95323f9fb12481783a928be
Sabbirhossen143 <sabbirhossen1430@gmail.com>
2026-07-01 17:46:48 +0600

    fixed localhost api urls

--- services/comments.ts
+++ services/comments.ts
@@ -1,17 +1,11 @@
 import axios from "axios";
 import axiosPublic from "@/lib/axios";
 
-export const createComment =
-async (
-  commentData: any
-) => {
-
-  const res =
-    await axios.post(
-      "http://localhost:5000/comments",
-      commentData
-    );
-
+export const createComment = async (commentData: any) => {
+  const res = await axiosPublic.post(
+    "/comments",
+    commentData
+  );
   return res.data;
 };
 
@@ -26,50 +20,34 @@ export const getUserComments =
     return res.data;
 };
 
-export const getCommentsByLawyer =
-  async (lawyerId: string) => {
-
-    const res =
-      await axios.get(
-        `http://localhost:5000/comments/lawyer/${lawyerId}`
-      );
-
-    return res.data;
+export const getCommentsByLawyer = async (lawyerId: string) => {
+  const res = await axiosPublic.get(
+    `/comments/lawyer/${lawyerId}`
+  );
+  return res.data;
 };
 
-export const getMyComments =
-  async (email: string) => {
-
-    const res =
-      await axios.get(
-        `http://localhost:5000/comments/user/${email}`
-      );
-
-    return res.data;
+export const getMyComments = async (email: string) => {
+  const res = await axiosPublic.get(
+    `/comments/user/${email}`
+  );
+  return res.data;
 };
 
-export const deleteComment =
-  async (id: string) => {
-
-    const res =
-      await axios.delete(
-        `http://localhost:5000/comments/${id}`
-      );
-
-    return res.data;
+export const deleteComment = async (id: string) => {
+  const res = await axiosPublic.delete(
+    `/comments/${id}`
+  );
+  return res.data;
 };
 
-export const updateComment =
-  async (
-    id: string,
-    comment: string
-  ) => {
-
-    const res =
-      await axios.patch(
-        `http://localhost:5000/comments/${id}`,
-        { comment }
-      );
-
-    return res.data;
+export const updateComment = async (
+  id: string,
+  comment: string
+) => {
+  const res = await axiosPublic.patch(
+    `/comments/${id}`,
+    { comment }
+  );
+  return res.data;
 };
--- services/transactions.ts
+++ services/transactions.ts
@@ -1,35 +1,27 @@
-import axios from "axios";
+import axiosPublic from "@/lib/axios";
 
-export const createTransaction =
-  async (data: any) => {
-
-    const res =
-      await axios.post(
-        "http://localhost:5000/transactions",
-        data
-      );
-
-    return res.data;
+export const createTransaction = async (data: any) => {
+  const res = await axiosPublic.post(
+    "/transactions",
+    data
+  );
+  return res.data;
 };
 
-export const getTransactionsByLawyer =
-  async (email: string) => {
-
-    const res =
-      await axios.get(
-        `http://localhost:5000/transactions/lawyer/${email}`
-      );
-
-    return res.data;
+export const getTransactionsByLawyer = async (
+  email: string
+) => {
+  const res = await axiosPublic.get(
+    `/transactions/lawyer/${email}`
+  );
+  return res.data;
 };
 
-export const getTransactionsByUser =
-  async (email: string) => {
-
-    const res =
-      await axios.get(
-        `http://localhost:5000/transactions/user/${email}`
-      );
-
-    return res.data;
+export const getTransactionsByUser = async (
+  email: string
+) => {
+  const res = await axiosPublic.get(
+    `/transactions/user/${email}`
+  );
+  return res.data;
 };
